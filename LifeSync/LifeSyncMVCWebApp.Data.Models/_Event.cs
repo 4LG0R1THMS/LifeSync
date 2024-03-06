e28d484e9e8e7f0ed200af87e14dc8935d6a6ddd
Borislav Chernev <borocool359@gmail.com>
2024-03-06 22:59:37 +0200

    Created Entity Models

---
+++ LifeSync/LifeSyncMVCWebApp.Data.Models/_Event.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeSyncMVCWebApp.Data.Models
+{
+    public class _Event
+    {
+        [Key]
+        public int EventId { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public string Location { get; set; } = null!;
+
+        // Navigation property
+        public _ApplicationUser User { get; set; } = null!;
+    }
+}
