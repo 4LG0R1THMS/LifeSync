a729316d75db67445242f8a066f5371da4025c00
Borislav Chernev <borocool359@gmail.com>
2024-03-14 17:44:20 +0200

    Added seeding to database

--- LifeSync/LifeSyncMVCWebApp.Data/Migrations/20240314154232_DatabaseSeeded.Designer.cs
+++ LifeSync/LifeSyncMVCWebApp.Data/Migrations/20240314154232_DatabaseSeeded.Designer.cs
@@ -4,6 +4,7 @@ using LifeSyncMVCWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LifeSyncMVCWebApp.Data.Migrations
 {
     [DbContext(typeof(LifeSyncDbContext))]
-    partial class LifeSyncDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240314154232_DatabaseSeeded")]
+    partial class DatabaseSeeded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
