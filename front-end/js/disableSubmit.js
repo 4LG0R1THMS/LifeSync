5bc2e1f1a011ad6fb3cfe4ae098eaf9f8ac11a1c
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2024-03-23 21:15:39 +0200

    Done with the register

---
+++ front-end/js/disableSubmit.js
@@ -0,0 +1,19 @@
+const form = document.getElementById('registrationForm');
+const submitButton = document.querySelector('.btn__submit');
+
+form.addEventListener('input', function() {
+const inputs = form.querySelectorAll('input[required]');
+let allFilled = true;
+
+inputs.forEach(input => {
+    if (!input.value.trim()) {
+        allFilled = false;
+    }
+});
+
+    if (allFilled) {
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
+    }
+});
