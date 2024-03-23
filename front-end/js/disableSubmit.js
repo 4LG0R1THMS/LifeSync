3462fccc3135e78c9f41bf24588da1d0636e256e
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2024-03-23 21:48:51 +0200

    Login page is done

--- front-end/js/disableSubmit.js
+++ front-end/js/disableSubmit.js
@@ -2,14 +2,14 @@ const form = document.getElementById('registrationForm');
 const submitButton = document.querySelector('.btn__submit');
 
 form.addEventListener('input', function() {
-const inputs = form.querySelectorAll('input[required]');
-let allFilled = true;
+    const inputs = form.querySelectorAll('input[required]');
+    let allFilled = true;
 
-inputs.forEach(input => {
-    if (!input.value.trim()) {
-        allFilled = false;
-    }
-});
+    inputs.forEach(input => {
+        if (!input.value.trim()) {
+            allFilled = false;
+        }
+    });
 
     if (allFilled) {
         submitButton.disabled = false;
